e928ba1dab030a545172d2f6b694b25c3d4a861d
Vyacheslav Nechepurenko <masterdog8@mail.ru>
2023-02-24 09:19:01 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,3 +1,18 @@
-public class PhoneBook {
+import java.util.HashMap;
+import java.util.Map;
 
+public class PhoneBook {
+   Map<Integer, String> contacts = new HashMap<>();
+
+   void add() {
+        for (Map.Entry<Integer, String> item : contacts.entrySet()) {
+            System.out.println("номер телефона: " + item.getKey() + "имя контакта: " + item.getValue());
+            return;
+        }
+    }
 }
+
+    // добавить имя с номером.
+    // метод должен возвращать количество контактов после добавления,
+    // при этом гарантируется, что не будут добавляться повторяющиеся имена
+
--- src/main/java/PhoneBookTest.java
+++ src/main/java/PhoneBookTest.java
@@ -1,3 +1,10 @@
+import org.testng.annotations.Test;
+
 public class PhoneBookTest {
 
+        //PhoneBook phoneBook = new PhoneBook();
+        @Test
+        public void add() { //Создать тест или тесты на метод findByNumber в классе тестов PhoneBookTest
+
+    }
 }
